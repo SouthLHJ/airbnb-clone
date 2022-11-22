4636f51b5b26464a9261f76d1fb6012cf28de435
SouthLHJ <107970775+SouthLHJ@users.noreply.github.com>
2022-11-22 20:17:47 +0900

    Update list.ts

--- pages/api/accommodation/list.ts
+++ pages/api/accommodation/list.ts
@@ -1,8 +1,10 @@
 import { NextApiHandler } from "next";
 import accommodations from "../../../lib/models/accommodations";
+import dbConnect from "../../../lib/models/connect";
 
 
 const handler : NextApiHandler = async(req,res)=>{
+    await dbConnect();
     try{
         const rcv = await accommodations.find({})
 
