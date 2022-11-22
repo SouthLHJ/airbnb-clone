bfc84b823f3639125ac280f884fe5363baf742ef
SouthLHJ <107970775+SouthLHJ@users.noreply.github.com>
2022-11-22 20:31:58 +0900

    Update list.ts

--- pages/api/accommodation/list.ts
+++ pages/api/accommodation/list.ts
@@ -7,7 +7,7 @@ const handler : NextApiHandler = async(req,res)=>{
     await dbConnect();
     try{
         const rcv = await accommodations.find({})
-
+        console.log(rcv)
         return res.status(200).json({result : true, datas: rcv})
     }catch(e:any){
         return res.status(422).json({result : false, error: e.message})
